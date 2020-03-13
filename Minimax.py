0d09697392642f6be0d9159db6baa8134e110f9b
UKDopham <alexandre34980@outlook.fr>
2020-03-13 12:50:10 +0100

    Add minimax

---
+++ Minimax.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 13 12:45:30 2020
+
+@author: Alexa
+"""
+
+class MiniMax:
+        
+    def minimax_decision(state):        
+        
+    def max_value(state):
+        
+    def mini_value(state):
--- Morpion.py
+++ Morpion.py
@@ -7,8 +7,23 @@ Created on Fri Mar 13 12:26:32 2020
 
 class Morpion:
     
-    def __init__(self):
+    def __init__(self, length):
+        self.length = length
         self.actions = []
+        self.matrix = self.createMatrix(length)
+        
+        
+        
+    def createMatrix(self, length): #Create the matrix with the length given in parameter
+        matrix = []
+        for i in range (0, length):
+            row = []
+            for j in range(0, length):
+                row.append(0)
+            matrix.append(row)
+        
+        return matrix
+    
     
         
         

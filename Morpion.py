6a705f11011be2869ad33801c533538c0751659c
UKDopham <alexandre34980@outlook.fr>
2020-03-13 12:35:18 +0100

    First push

---
+++ Morpion.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 13 12:26:32 2020
+
+@author: Alexa
+"""
+
+class Morpion:
+    
+    def __init__(self):
+        self.actions = []
+    
+        
+        
